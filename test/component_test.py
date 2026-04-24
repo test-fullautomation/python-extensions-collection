93373a8c864a673886e05c9a9e2b889dca90880f
qth2gi <Holger.Queckenstedt@de.bosch.com>
2026-04-24 19:04:04 +0200

    HTML documentation activated in GenPackageDoc configuration; maintenance

--- test/component_test.py
+++ test/component_test.py
@@ -60,8 +60,8 @@ from PythonExtensionsCollection.Folder.CFolder import CFolder
 from PythonExtensionsCollection.File.CFile import CFile
 from PythonExtensionsCollection.Utils.CUtils import *
 
-from PythonExtensionsCollection.version import __version__ as SUT_VERSION
-from PythonExtensionsCollection.version import _version_date as SUT_VERSION_DATE
+from PythonExtensionsCollection.version import VERSION as SUT_VERSION
+from PythonExtensionsCollection.version import VERSION_DATE as SUT_VERSION_DATE
 
 from libs.CConfig import CConfig
 from libs.CCodePatterns import CCodePatterns
